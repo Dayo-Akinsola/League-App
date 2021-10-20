18cf431c77220d85157f32fbf44d0d41bc0b83e7
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-10-20 15:30:22 +0100

    removed class use

--- src/championData.js
+++ src/championData.js
@@ -1,15 +1,47 @@
-export default class ChampionData {
-  constructor(championId) {
-    return (async () => {
-      const request = await fetch(`http://ddragon.leagueoflegends.com/cdn/11.20.1/data/en_US/champion/${championId}.json`, { mode: 'cors' });
+const fetchChampionDetails = async (championId) => {
+  const response = await fetch(`http://ddragon.leagueoflegends.com/cdn/11.20.1/data/en_US/champion/${championId}.json`, { mode: 'cors' });
+  const champions = await response.json();
+  const championDetails = champions.data[championId];
 
-      const response = await request.json();
+  return championDetails;
+};
 
-      const data = response.data.championId;
+const fetchChampionImages = async (championId) => {
+  const championDetails = await fetchChampionDetails(championId);
 
-      this.getChampionName = () => data.name;
+  const profileImgNumber = championDetails.skins[0].num;
+  const profileImgUrl = (`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${championId}_${profileImgNumber}.jpg`);
 
-      return this;
-    })();
-  }
-}
+  const skinImgs = championDetails.skins.slice(1);
+  console.log(skinImgs);
+  const skinImgUrls = [];
+  skinImgs.forEach((img) => {
+    skinImgUrls.push(`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${championId}_${img.num}.jpg`);
+  });
+
+  return {
+    profileImg: profileImgUrl,
+    skinImgs: skinImgUrls,
+  };
+};
+
+const getChampionData = async (championId) => {
+  const championDetails = await fetchChampionDetails(championId);
+  const championImages = await fetchChampionImages(championId);
+
+  return {
+    name: championDetails.name,
+    title: championDetails.title,
+    lore: championDetails.lore,
+    role: championDetails.tags[0],
+    passive: championDetails.passive,
+    abilities: championDetails.spells,
+    profileImg: championImages.profileImg,
+    skinImgs: championImages.skinImgs,
+  };
+};
+
+export {
+  // eslint-disable-next-line import/prefer-default-export
+  getChampionData,
+};
