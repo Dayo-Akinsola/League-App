2f8c3a30979caed1cd203b023a27c25100bad2c4
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-10-20 15:29:06 +0100

    @babel/transform-runtime included to allow async functions

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,35 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    devtool: 'inline-source-map',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: 'defaults' }],
+            ],
+            plugins: [
+              ['@babel/plugin-proposal-class-properties'],
+              ['@babel/transform-runtime'],
+            ],
+          },
+        },
+      },
+    ],
+  },
 };
