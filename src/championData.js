bd2dff1abdef310b7a12c5eecb728dcae6efa96e
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-10-21 01:49:13 +0100

    Added functions to fetch champion data

--- src/championData.js
+++ src/championData.js
@@ -6,17 +6,15 @@ const fetchChampionDetails = async (championId) => {
   return championDetails;
 };
 
-const fetchChampionImages = async (championId) => {
-  const championDetails = await fetchChampionDetails(championId);
-
+const getChampionImages = async (championDetails) => {
   const profileImgNumber = championDetails.skins[0].num;
-  const profileImgUrl = (`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${championId}_${profileImgNumber}.jpg`);
+  const profileImgUrl = (`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${championDetails.id}_${profileImgNumber}.jpg`);
 
   const skinImgs = championDetails.skins.slice(1);
   console.log(skinImgs);
   const skinImgUrls = [];
   skinImgs.forEach((img) => {
-    skinImgUrls.push(`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${championId}_${img.num}.jpg`);
+    skinImgUrls.push(`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${championDetails.id}_${img.num}.jpg`);
   });
 
   return {
@@ -25,19 +23,54 @@ const fetchChampionImages = async (championId) => {
   };
 };
 
+const getChampionPassives = (championDetails) => {
+  const passiveData = championDetails.passive;
+  const passiveName = passiveData.name;
+  const passiveDescription = passiveData.description;
+  const passiveId = passiveData.image.full;
+  const passiveImgUrl = `https://ddragon.leagueoflegends.com/cdn/11.21.1/img/passive/${passiveId}`;
+
+  return {
+    passiveName,
+    passiveDescription,
+    passiveImgUrl,
+  };
+};
+
+const getChampionSpells = (championDetails) => {
+  const spellsData = championDetails.spells;
+  const spells = [];
+
+  spellsData.forEach((spell) => {
+    const spellObject = {
+      name: spell.name,
+      description: spell.description,
+      costBurn: spell.costBurn,
+      costType: spell.costType,
+      cooldown: spell.cooldownBurn,
+      imgUrl: `https://ddragon.leagueoflegends.com/cdn/11.21.1/img/spell/${spell.id}.png`,
+    };
+    spells.push(spellObject);
+  });
+
+  return spells;
+};
+
 const getChampionData = async (championId) => {
   const championDetails = await fetchChampionDetails(championId);
-  const championImages = await fetchChampionImages(championId);
+  const championImages = getChampionImages(championDetails);
+  const championAbilities = getChampionPassives(championDetails);
+  const championSpells = getChampionSpells(championDetails);
 
   return {
     name: championDetails.name,
     title: championDetails.title,
     lore: championDetails.lore,
     role: championDetails.tags[0],
-    passive: championDetails.passive,
-    abilities: championDetails.spells,
+    passive: championAbilities,
     profileImg: championImages.profileImg,
     skinImgs: championImages.skinImgs,
+    championSpells,
   };
 };
 
