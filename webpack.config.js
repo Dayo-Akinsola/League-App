2f7fd594ddc4765240fd1d8c55b998345455f3ee
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-10-21 01:49:34 +0100

    Included support for sass

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,12 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
       },
       {
         test: /\.m?js$/,
