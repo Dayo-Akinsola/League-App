dfb6ae3b9d5aa33ada1fe0d9c0c790cf72f1861f
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-10-25 18:55:50 +0100

    testing caching api callls

--- backend/apiCalls.js
+++ backend/apiCalls.js
@@ -8,10 +8,11 @@ const cache = new Cache(ttl);
 
 const ApiCalls = (tier, division) => {
 
+  //Collects summoner ids for players in a given tier and division
   const getSummonerIds = async () => { 
     const requestUrl = `https://euw1.api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/${tier}/${division}?page=1&api_key=${process.env.API_KEY}`;
     const key = `getSummonerIds_${tier + division}`;
-    const response = cache.get(key, async () => await axios.get(requestUrl));
+    const response = await axios.get(requestUrl);
     const playerData = response.data;
     const summonerIds = playerData.map(player => player.summonerId);
 
@@ -21,7 +22,8 @@ const ApiCalls = (tier, division) => {
   const getUniqueId = async (summonerId) => {
 
     const requestUrl = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/${summonerId}?api_key=${process.env.API_KEY}`;
-    const response = cache.get(key, async () => await axios.get(requestUrl));
+    const key = `getUniqueId_${summonerId}`;
+    const response = await cache.get(key, async () => await axios.get(requestUrl));
     const playerInfo = response.data;
     const uniqueId = playerInfo.puuid;
 
