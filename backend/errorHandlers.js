6343cff92e75de2c4dd262302001248ef198b57c
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-10-28 23:23:47 +0100

    created to handle 404 and 429 api responses

---
+++ backend/errorHandlers.js
@@ -0,0 +1,27 @@
+const sleep = (seconds) => {
+  return new Promise((resolve) => setTimeout(resolve, seconds));
+}
+
+const getSecondsToSleep = (retryAfter) => {
+  const secondsToSleep = parseInt(retryAfter) * 1000;
+  return secondsToSleep;
+}
+
+const fetchAndRetryIfNecessary = async (apiCall) => {
+  const response = await apiCall();
+
+  if (response.status === 429) {
+    const retryAfter = response.headers['retry-after'];
+    const secondsToSleep = getSecondsToSleep(retryAfter);
+    await sleep(secondsToSleep);
+    return fetchAndRetryIfNecessary(apiCall);
+  }
+
+  if (response.status === 404) {
+    throw 'Match does not exist';
+  }
+
+  return response;
+}
+
+module.exports = fetchAndRetryIfNecessary;
