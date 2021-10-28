0a50ec9ccf2e5383205a39619d38b7a36ae728b5
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-10-28 23:25:31 +0100

    removed throttle class instance

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
-const Throttle = require('./throttle');
+const throttledQueue = require('throttled-queue');
 const ApiCalls = require('./apiCalls');
 const Player = require('./models/player');
 const Match = require('./models/match');
-const fetchAndRetryIfNecessary = require('./errorHandlers')
+const fetchAndRetryIfNecessary = require('./errorHandlers');
+const ChampionStats = require('./getChampionStats');
 
 // Makes 99 evenly spaced requests every 2 minutes
-const throttle = new Throttle(99, 120);
+const throttle = throttledQueue(99, 120 * 1000, true);
 
 const platIVApiCalls = ApiCalls('PLATINUM', 'IV');
 
@@ -19,73 +20,65 @@ app.get('/api/players', async (request, response) => {
   for ( let i = 0; i < summonerIds.length; i++) {
     const isPlayerinDb = await Player.exists({summonerId: summonerIds[i]});
     
-    const uniqueId = await throttle.getRequestResponse(() => platIVApiCalls.getUniqueId(summonerIds[i]));
     if (!isPlayerinDb) {
       throttle( async () => {
         const uniqueId = await platIVApiCalls.getUniqueId(summonerIds[i]);
-        console.log(`${i}: ${uniqueId}`);
+        const playerMatchIds = await fetchAndRetryIfNecessary(() => platIVApiCalls.getPlayerMatchIds(uniqueId));
+
+        console.log(`${i}: ${uniqueId}, ${playerMatchIds}`);
 
         const player = new Player({
           summonerId: summonerIds[i],
           uniqueId: uniqueId,
+          matchIds: playerMatchIds,
         });
 
         await player.save();
         console.log('player saved');
       });
     }
-
     else{
       console.log('player already in db');
     }
-    
   };
-
-  //const rate_limits = axios_response.headers['x-app-rate-limit'].split(',');
-  //const limits = rate_limits.map(limit => limit.split(':'));
-  //const limitPerSecond = parseInt(limits[0][0]);
-  //const limitPerTwoMinutes = parseInt(limits[1][1]);
-  //console.log(limitPerSecond);
-  //console.log(limitPerTwoMinutes);
 });
 
 app.get('/api/matches', async (request, response) => {
   const players = await Player.find({});
 
-  for (let i = 0; i < players.length; i++) {
+  for (const player of players) {
+    const playerMatchIds = player.matchIds;
 
-    throttle( async () => {
-      const playerMatchIds = await fetchAndRetryIfNecessary(() => platIVApiCalls.getPlayerMatchIds(players[i].uniqueId));
-      console.log(playerMatchIds);
-    })
-
-
-    for (let i = 0; i < playerMatchIds.length; i++) {
-      try{
-        const isMatchInDb =  await Match.exists({matchId: playerMatchIds[i]});
-        if (!isMatchInDb) {
-          const matchData = await fetchAndRetryIfNecessary(() => platIVApiCalls.getMatchData(playerMatchIds[i]));
-
-          const match = new Match({
-            matchId: playerMatchIds[i],
-            matchData: matchData,
-          });
-
-          await match.save();
-          console.log('match saved in the database');
-        } 
-        else {
-          console.log('match is already in the database');
-        }
-      } 
-      catch {
-        if (response.status === 404) {
-          console.log('match not found');
-          continue;
-        }
+    for (const matchId of playerMatchIds) {
+      const isMatchInDb = await Match.exists({matchId: matchId});
+      if (!isMatchInDb) {
+        throttle( async () => {
+          try{
+            const matchData = await fetchAndRetryIfNecessary(() => platIVApiCalls.getMatchData(matchId));
+          
+            const match = new Match({
+              matchId,
+              matchData,
+            })
+            await match.save();
+            console.log('match saved'); 
+          }
+          catch(e) {
+            console.log(e);
+          }
+          
+        })
+      }
+      else {
+        console.log('match already in database');
       }
     }
   }
+})
+
+app.get('/champions', async (request, response) => {
+  const champion = new ChampionStats('Jinx');
+  console.log(await champion.getChampionStats());
 })
 
 const PORT = 3001;
