c37bef6f531897acc75b857b78e06efd0093b8c9
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-11-14 19:08:51 +0000

    included route to get all champions from the db

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,16 @@ const Champion = require('./models/champion');
 app.get('/:championName', async (request, response) => {
   response.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
   const champion = await Champion.findOne({championName: request.params.championName});
+  console.log(champion);
   response.json(champion);
 });
+
+app.get('/champions', async (request, response) => {
+  response.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+  const allChampions = await Champion.find({});
+  response.json(allChampions);
+  console.log(allChampions);
+})
 
 
 const PORT = 3001;
