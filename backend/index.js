56410cadf8229e3ad40a59b18167fd021cf7d85e
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-11-23 18:01:37 +0000

    added route to get data for all champions

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,33 @@
 const express = require('express');
 const app = express();
 const Champion = require('./models/champion');
-//const saveChampionsToDb = require('./storage');
+const Match = require('./models/match');
+// const saveChampionsToDb = require('./storage');
 
-//saveChampionsToDb();
-
-app.get('/:championName', async (request, response) => {
-  response.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
-  const champion = await Champion.findOne({championName: request.params.championName});
-  console.log(champion);
-  response.json(champion);
-});
 
 app.get('/champions', async (request, response) => {
   response.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
   const allChampions = await Champion.find({});
   response.json(allChampions);
-  console.log(allChampions);
-})
+});
 
 
-const PORT = 3001;
+app.get('/:championId', async (request, response) => {
+  response.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+  try {
+    const champion = await Champion.findOne({id: request.params.championId});
+    console.log(champion);
+    response.json(champion);
+  }
+  catch {
+    response.status(404).send({ error: 'Champion does not exist'});
+  }
+ 
+});
+
+
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
