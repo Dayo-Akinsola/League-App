5937da4a8350ccc227a4d2b59a7075d42bfbd2d8
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-11-24 00:02:22 +0000

    allowed cors access for all urls

--- backend/index.js
+++ backend/index.js
@@ -7,14 +7,16 @@ app.use(express.static('dist'));
 
 
 app.get('/champions', async (request, response) => {
-  response.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+  console.log(request.protocol);
+  console.log(request.get('host'));
+  response.setHeader('Access-Control-Allow-Origin', '*');
   const allChampions = await Champion.find({});
   response.json(allChampions);
 });
 
 
 app.get('/:championId', async (request, response) => {
-  response.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+  response.setHeader('Access-Control-Allow-Origin', '*');
   try {
     const champion = await Champion.findOne({id: request.params.championId});
     console.log(champion);
