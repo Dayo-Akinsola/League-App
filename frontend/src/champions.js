2a8a19e23429effb3b0ba21fb7a3e3975918a525
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-12-03 02:12:38 +0000

    added set timeout on page load

--- frontend/src/champions.js
+++ frontend/src/champions.js
@@ -1,24 +1,19 @@
 import './stylesheets/modals.scss';
 import './stylesheets/champions.scss';
-// import { championDetailsModal } from './templates/championDetailsModal';
-// import championStatsModal from './templates/championStatsModal';
-// import ItemInfo from './championData/itemInfo';
-// import getChampionDetails from './championData/championDetails';
-// import championSkinsModal from './templates/championSkinsModal';
-// import getChampionStats from './championData/championStats';
 import championsPage from './templates/championsPage';
 import ChampionsPageController from './appControl/championsPageController';
 import SearchbarController from './appControl/searchbarController';
 
-const test = async () => {
-  const response = await fetch('https://ddragon.leagueoflegends.com/cdn/11.22.1/data/en_US/champion.json');
-  const allChampions = await response.json();
-
-  // const values = Object.values(allChampions.data);
-  // values.forEach((value) => console.log(value.info.difficulty));
-  await championsPage();
-  ChampionsPageController.championsPageListeners();
-  SearchbarController.searchContainerListeners();
-  // window.history.pushState('champions', '', '/champions');
+const createChampionsPage = () => {
+  setTimeout(async () => {
+    await championsPage();
+    ChampionsPageController.championsPageListeners();
+    SearchbarController.searchContainerListeners();
+    const championId = localStorage.getItem('championId');
+    if (championId) {
+      ChampionsPageController.displayChampionModal(championId);
+      localStorage.clear();
+    }
+  }, 300);
 };
-test();
+createChampionsPage();
