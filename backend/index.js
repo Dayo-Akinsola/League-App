40e8914163ab98beb5fbbb633c2ea9d5e76ccfd4
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-12-03 22:10:27 +0000

    added route to get number of matches

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const Champion = require('./models/champion');
+const Match = require('./models/match');
 app.use(express.static('dist'));
 
 app.get('/champions', async (request, response) => {
@@ -9,18 +10,24 @@ app.get('/champions', async (request, response) => {
   response.json(allChampions);
 });
 
+app.get('/matches', async (request, response) => {
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  const matchCount = await Match.collection.countDocuments();
+  response.json(matchCount);
+});
 
 app.get('/:championId', async (request, response) => {
   response.setHeader('Access-Control-Allow-Origin', '*');
   try {
     const champion = await Champion.findOne({id: request.params.championId});
-    console.log(champion);
     response.json(champion);
   }
   catch {
     response.status(404).send({ error: 'Champion does not exist'});
   }
 });
+
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
