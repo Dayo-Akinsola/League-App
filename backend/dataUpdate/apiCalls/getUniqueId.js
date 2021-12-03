f4692379190d74adcf6244098005165e81f79d71
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-12-03 02:05:56 +0000

    removed whitespace

--- backend/dataUpdate/apiCalls/getUniqueId.js
+++ backend/dataUpdate/apiCalls/getUniqueId.js
@@ -3,10 +3,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = getUniqueId = async (summonerId) => {
-
-    const requestUrl = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/${summonerId}?api_key=${process.env.API_KEY}`;
-    const response = await axios.get(requestUrl);
-    const uniqueId = response.data.puuid;  
-    return uniqueId;
+  const requestUrl = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/${summonerId}?api_key=${process.env.API_KEY}`;
+  const response = await axios.get(requestUrl);
+  const uniqueId = response.data.puuid;  
+  return uniqueId;
 }
 
